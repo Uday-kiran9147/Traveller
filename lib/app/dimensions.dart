9278acd9d64269e0e1c976a17d610aeb040042ac
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-09-29 12:37:55 +0530

    removed lib folder

--- lib/app/dimensions.dart
+++
@@ -1,2 +0,0 @@
-
-const webScr=600;
--- lib/presentation/pages/home/bloc/home_bloc_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_bloc_bloc.dart';
-
-@immutable
-abstract class HomeBlocEvent {}
-
-class HomeInitialEvent extends HomeBlocEvent {}
-
-class NavigateToExploreEvent extends HomeBlocEvent {}
